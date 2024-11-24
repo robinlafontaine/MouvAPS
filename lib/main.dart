76ee0fb67b9c327c860b9a5469f45bba6b97a1ba
Robin Lafontaine <46829268+robinlafontaine@users.noreply.github.com>
2024-11-25 00:16:40 +0100

    changed HomePage title

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: "Mouv'APS v0.0.1"),
+      home: const MyHomePage(title: "Mouv'APS v0.0.1 "),
     );
   }
 }
