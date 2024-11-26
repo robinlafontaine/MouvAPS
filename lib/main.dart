6a8b8817d178ab403570f706c33ddeead959ae2c
robinlafontaine <robin@lafontaines.eu>
2024-11-26 16:03:11 +0100

    fix(main): removed google_fonts that was causing build errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
-import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,30 +12,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ShadApp.material(
-      title: 'Shadcn Demo',
+      materialThemeBuilder: (context, theme) {
+        return theme.copyWith(
+          appBarTheme: const AppBarTheme(toolbarHeight: 52),
+        );
+      },
       theme: ShadThemeData(
-        brightness: Brightness.light,
         colorScheme: const ShadBlueColorScheme.light(),
-        textTheme: ShadTextTheme.fromGoogleFont(
-          GoogleFonts.poppins
-        ),
-      ),
+        brightness: Brightness.light,
+//        textTheme: ShadTextTheme.fromGoogleFont(
+//        GoogleFonts.poppins
+//      ),
+    ),
+      title: 'Shadcn Demo',
       home: const MyHomePage(title: "Mouv'APS Demo"),
+
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
