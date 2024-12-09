c66da1b5b922b387b8228c0d93efc663a42b99aa
bastub <bastub57@gmail.com>
2024-12-09 15:09:54 +0100

    feat(admin): add admin menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:mouvaps/auth_controller.dart';
-import 'package:mouvaps/home_screen.dart';
-import 'package:mouvaps/signin_screen.dart';
+import 'package:mouvaps/admin/home_screen.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'otp_screen.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
@@ -38,10 +35,7 @@ class MyApp extends StatelessWidget {
       title: "Mouv'APS",
       initialRoute: '/',
       routes: {
-        '/': (context) => const AuthController(),
-        '/home': (context) => const HomeScreen(),
-        '/signin': (context) => const SignInScreen(),
-        '/otp': (context) => OTPScreen(email: ModalRoute.of(context)!.settings.arguments as String),
+        '/': (context) => const HomeScreen()
       },
     );
   }
