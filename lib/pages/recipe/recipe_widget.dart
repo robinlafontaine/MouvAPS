2b10ccd2bda7f7c854782913822946415a3a6264
Liam Boudadi <bou.liamou@gmail.com>
2025-01-13 17:16:43 +0100

    chore: remove useless functions

--- lib/pages/recipe/recipe_widget.dart
+++ lib/pages/recipe/recipe_widget.dart
@@ -1,15 +1,12 @@
 import 'dart:core';
 import 'dart:io';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mouvaps/pages/recipe/locked_recipe_details_screen.dart';
 import 'package:mouvaps/pages/recipe/recipe_details_screen.dart';
 import 'package:mouvaps/services/recipe.dart';
-import 'package:provider/provider.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 import 'package:mouvaps/utils/constants.dart';
 import 'package:mouvaps/services/user.dart';
-import 'package:mouvaps/notifiers/user_points_notifier.dart';
 import 'package:mouvaps/services/download.dart';
 import 'package:mouvaps/widgets/download_button.dart';
 
@@ -45,6 +42,7 @@ class _RecipeWidgetState extends State<RecipeWidget> {
       leading: _buildRecipeImage(),
       title: _buildRecipeTitle(),
       subtitle: _buildRecipeSubtitle(),
+      trailing: !widget.isLocked ? _buildDownloadButton() : null,
       onTap: _handleRecipeTap,
     );
   }
@@ -94,108 +92,6 @@ class _RecipeWidgetState extends State<RecipeWidget> {
     );
   }
 
-  Widget _buildUnlockButton() {
-    if (widget.user == null) {
-      return const CircularProgressIndicator();
-    }
-    return FutureBuilder<User>(
-      future: widget.user,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else if (snapshot.hasData) {
-          return _buildUnlockButtonContent(snapshot.data!);
-        }
-        return Container();
-      },
-    );
-  }
-
-  Widget _buildUnlockButtonContent(User userData) {
-    if (userData == User.empty()) {
-      return const SizedBox();
-    }
-    bool canUnlock = userData.points >= (widget.recipe.pricePoints ?? 0);
-    return IconButton(
-      onPressed: canUnlock ? () => _handleUnlock(userData) : null,
-      icon: Icon(
-        Icons.lock_open,
-        color: canUnlock ? primaryColor : Colors.grey,
-      ),
-    );
-  }
-
-  Future<void> _handleUnlock(User userData) async {
-    if (userData == User.empty()) {
-      return;
-    }
-    bool? result = await _showUnlockConfirmationDialog();
-    if (result == true) {
-      await _processUnlock(userData);
-    }
-  }
-
-  Future<bool?> _showUnlockConfirmationDialog() {
-    return showDialog<bool>(
-      context: context,
-      builder: (BuildContext context) => _buildUnlockDialog(),
-    );
-  }
-
-  Widget _buildUnlockDialog() {
-    return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      elevation: 5.0,
-      backgroundColor: Colors.white,
-      child: Container(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Déverrouiller ${widget.recipe.name} pour ${widget.recipe.pricePoints} pts ?',
-              style: ShadTheme.of(context).textTheme.p,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            _buildUnlockDialogButtons(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildUnlockDialogButtons() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        ShadButton(
-          onPressed: () => Navigator.of(context).pop(true),
-          child: const Text('Oui'),
-        ),
-        ShadButton(
-          onPressed: () => Navigator.of(context).pop(false),
-          child: const Text('Annuler'),
-        ),
-      ],
-    );
-  }
-
-  Future<void> _processUnlock(User userData) async {
-    await widget.recipe.unlockRecipe(userData.userUuid);
-    await userData
-        .updatePoints(userData.points - (widget.recipe.pricePoints ?? 0));
-    if (!mounted) return;
-    Provider.of<UserPointsNotifier>(context, listen: false)
-        .addPoints(-(widget.recipe.pricePoints ?? 0));
-    widget.onRecipeStateChanged();
-  }
-
   Widget _buildDownloadButton() {
     return DownloadButton<Recipe>(
       item: widget.recipe,
@@ -226,64 +122,6 @@ class _RecipeWidgetState extends State<RecipeWidget> {
         if (!mounted) return;
         setState(() {});
       },
-    );
-  }
-
-  Widget _buildOfflineIndicator() {
-    return const IconButton(
-      onPressed: null,
-      icon: Icon(
-        Icons.check_circle_outlined,
-        color: primaryColor,
-      ),
-    );
-  }
-
-  Widget _ingredientButton() {
-    return IconButton(
-      onPressed: _showIngredientsDialog,
-      icon: const Icon(
-        Icons.menu_book,
-        color: primaryColor,
-      ),
-    );
-  }
-
-  void _showIngredientsDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) => _buildIngredientsDialog(),
-    );
-  }
-
-  Widget _buildIngredientsDialog() {
-    return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      elevation: 5.0,
-      backgroundColor: Colors.white,
-      child: Container(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Ingrédients',
-              style: ShadTheme.of(context).textTheme.h3,
-            ),
-            const SizedBox(height: 10),
-            Column(
-              children: widget.recipe.ingredients!
-                  .map((ingredient) => Text(
-                        ingredient.name,
-                        style: ShadTheme.of(context).textTheme.p,
-                      ))
-                  .toList(),
-            ),
-          ],
-        ),
-      ),
     );
   }
 
